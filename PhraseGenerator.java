4cd83832de602f0de51a203553631e4534cbc98e
lennytmp <lennytmp@gmail.com>
2017-10-14 21:37:21 +0100

    Log exceptions when translations are not found, but not fail

--- PhraseGenerator.java
+++ PhraseGenerator.java
@@ -4,30 +4,32 @@ import java.util.HashMap;
 import java.util.Map;
 
 class PhraseGenerator {
+  static String[] languages = {"en", "ru"};
 
   static Map<String, String> getJoinedTheFightClub(String username) {
     Map<String, String> result = new HashMap<>();
-    for (String lang : Phrases.miscTexts.joinedTheFightClub.keySet()) {
+    for (String lang : languages) {
       result.put(lang, username + " " +
-        Phrases.miscTexts.joinedTheFightClub.get(lang));
+        getTranslation(Phrases.miscTexts.joinedTheFightClub, lang));
     }
     return result;
   }
 
   static Map<String, String> getReadyToFightPhrase(Client client) {
     Map<String, String> result = new HashMap<>();
-    for (String lang : Phrases.combatTexts.lookingForOpponent.keySet()) {
+    for (String lang : languages) {
       result.put(lang, "\u2694 " + client.username + " " +
-        Utils.getRnd(Phrases.combatTexts.lookingForOpponent.get(lang)) + ".");
+        Utils.getRnd(getTranslation(
+            Phrases.combatTexts.lookingForOpponent, lang)) + ".");
     }
     return result;
   }
 
   static Map<String, String> getWonPhrase(Client winner, Client loser) {
     Map<String, String> result = new HashMap<>();
-    for (String lang : Phrases.combatTexts.lookingForOpponent.keySet()) {
+    for (String lang : languages) {
       result.put(lang, "\u2620 " + winner.username + " " +
-        Utils.getRnd(Phrases.combatTexts.won.get(lang)) + " " +
+        Utils.getRnd(getTranslation(Phrases.combatTexts.won, lang)) + " " +
         loser.username + ".");
     }
     return result;
@@ -35,10 +37,12 @@ class PhraseGenerator {
 
   static Map<String, String> getWisdom(Client client) {
     Map<String, String> result = new HashMap<>();
-    for (String lang : Phrases.combatTexts.lookingForOpponent.keySet()) {
+    for (String lang : languages) {
       result.put(lang,
-        Utils.getRnd(Phrases.wiseTexts.wisdomIntro.get(client.lang)) +
-        " " + Utils.getRnd(Phrases.wiseTexts.wisdoms.get(client.lang)));
+        Utils.getRnd(getTranslation(Phrases.wiseTexts.wisdomIntro,
+            client.lang)) + " " +
+        Utils.getRnd(getTranslation(Phrases.wiseTexts.wisdoms,
+            client.lang)));
     }
     return result;
   }
@@ -49,16 +53,15 @@ class PhraseGenerator {
                              boolean critHit,
                              int damage) {
     Map<String, String> result = new HashMap<>();
-    for (String lang : Phrases.combatTexts.lookingForOpponent.keySet()) {
+    for (String lang : languages) {
       // Don't think about StringBuilder, it's a lie.
       // https://stackoverflow.com/questions/4965513/stringbuilder-vs-string-considering-replace 
-      String tmp =
-          victim.username + " " +
-        Utils.getRnd(Phrases.combatTexts.wasDoingSomething.get(lang)) +
-        ", " + Utils.getRnd(Phrases.combatTexts.when.get(lang)) + " " +
-        Utils.getRnd(Phrases.combatTexts.adjective.get(lang)) + " " +
-        offender.username +  " " +
-        Utils.getRnd(Phrases.combatTexts.hit.get(lang)[part.ordinal()]) + ". ";
+      String tmp = victim.username + " " + Utils.getRnd(getTranslation(
+            Phrases.combatTexts.wasDoingSomething, lang)) + ", " +
+        Utils.getRnd(getTranslation(Phrases.combatTexts.when, lang)) + " " +
+        Utils.getRnd(getTranslation(Phrases.combatTexts.adjective, lang)) +
+        " " + offender.username +  " " + Utils.getRnd(getTranslation(
+            Phrases.combatTexts.hit, lang)[part.ordinal()]) + ". ";
       if (critHit) {
         tmp += "Critical hit! ";
       }
@@ -72,14 +75,15 @@ class PhraseGenerator {
                                Client victim,
                                Client.BodyPart part) {
     Map<String, String> result = new HashMap<>();
-    for (String lang : Phrases.combatTexts.lookingForOpponent.keySet()) {
+    for (String lang : languages) {
       result.put(lang, offender.username + " " + 
-        Utils.getRnd(Phrases.combatTexts.wasTrying.get(lang)) + " " +
-        Utils.getRnd(Phrases.combatTexts.toHit.get(lang)[part.ordinal()]) +
-        " " + Utils.getRnd(Phrases.combatTexts.but.get(lang)) + " " +
-        victim.username +  " " +
-        Utils.getRnd(Phrases.combatTexts.blocked.get(lang)) + ". " +
-        "[" + victim.hp + "/" + victim.getMaxHp() + "]");
+        Utils.getRnd(getTranslation(Phrases.combatTexts.wasTrying, lang)) +
+        " " + Utils.getRnd(getTranslation(
+            Phrases.combatTexts.toHit, lang)[part.ordinal()]) +
+        " " + Utils.getRnd(getTranslation(Phrases.combatTexts.but, lang)) +
+        " " + victim.username +  " " +
+        Utils.getRnd(getTranslation(Phrases.combatTexts.blocked, lang)) +
+        ". " + "[" + victim.hp + "/" + victim.getMaxHp() + "]");
     }
     return result;
   }
@@ -88,13 +92,14 @@ class PhraseGenerator {
                               Client victim,
                               Client.BodyPart part) {
     Map<String, String> result = new HashMap<>();
-    for (String lang : Phrases.combatTexts.lookingForOpponent.keySet()) {
+    for (String lang : languages) {
       result.put(lang, offender.username + " " +
-        Utils.getRnd(Phrases.combatTexts.wasTrying.get(lang)) + " " +
-        Utils.getRnd(Phrases.combatTexts.toHit.get(lang)[part.ordinal()]) +
-        " " + Utils.getRnd(Phrases.combatTexts.but.get(lang)) +  " " +
-        Utils.getRnd(Phrases.combatTexts.missed.get(lang)) + ". " +
-        "[" + victim.hp + "/" + victim.getMaxHp() + "]");
+        Utils.getRnd(getTranslation(Phrases.combatTexts.wasTrying, lang)) +
+        " " + Utils.getRnd(getTranslation(
+            Phrases.combatTexts.toHit, lang)[part.ordinal()]) +
+        " " + Utils.getRnd(getTranslation(Phrases.combatTexts.but, lang)) +
+        " " + Utils.getRnd(getTranslation(Phrases.combatTexts.missed, lang)) +
+        ". " + "[" + victim.hp + "/" + victim.getMaxHp() + "]");
     }
     return result;
   }
@@ -102,11 +107,23 @@ class PhraseGenerator {
 
   static Map<String, String> getSayingPhrase(Client teller, String msg, Client listener) {
     Map<String, String> result = new HashMap<>();
-    for (String lang : Phrases.combatTexts.lookingForOpponent.keySet()) {
+    for (String lang : languages) {
       result.put(lang, teller.username + " " +
-        Utils.getRnd(Phrases.combatTexts.said.get(lang)) + " " +
+        Utils.getRnd(getTranslation(Phrases.combatTexts.said, lang)) + " " +
         listener.username + ": " + msg);
     }
     return result;
+  }
+
+  private static <K> K getTranslation(Map<String, K> translations,
+                                       String targetLang) {
+    if (translations.get(targetLang) == null) {
+      if (translations.get("en") == null) {
+        Logger.logException(new Exception("No language for the phrase:" +
+            translations.toString()));
+      }
+      return translations.get("en");
+    }
+    return translations.get(targetLang);
   }
 };
